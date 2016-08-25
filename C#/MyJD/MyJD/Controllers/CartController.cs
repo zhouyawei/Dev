e7fde00281fcf0f68c17b6726baa539bd4013813
zhouyawei <1072817424@qq.com>
2016-08-25 15:40:37 +0800

    忽略bin和obj目录
    忽略bin和obj目录

--- C#/MyJD/MyJD/Controllers/CartController.cs
+++ C#/MyJD/MyJD/Controllers/CartController.cs
@@ -10,13 +10,27 @@ namespace MyJD.Controllers
     {
         //添加产品项目到购物车, 如果没有传入Amount参数则默认购买数量为1
         [HttpPost]
-        public ActionResult AddToCart(int productId, int Amount = 1)
+        public ActionResult AddToCart(int productId, int amount = 1)
         {
             return View();
         }
 
         //显示当前的购物车项目
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        //移除购物车项目
+        [HttpPost]
+        public ActionResult Remove()
+        {
+            return View();
+        }
+
+        //更新购物车中特定项目的数量
+        [HttpPost]
+        public ActionResult UpdateAmount(int productId, int newAmount)
         {
             return View();
         }
