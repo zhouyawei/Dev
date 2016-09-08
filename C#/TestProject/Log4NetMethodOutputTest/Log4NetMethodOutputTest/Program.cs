fdc627520539cb7ff3fbc3627ee4b78433330a89
zhouyawei <1072817424@qq.com>
2016-09-08 16:30:52 +0800

    添加测试工程

---
+++ C#/TestProject/Log4NetMethodOutputTest/Log4NetMethodOutputTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+
+[assembly: XmlConfigurator(Watch = true)]
+namespace Log4NetMethodOutputTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILog log = LogManager.GetLogger(typeof(Program));
+            log.Info("输出方法名测试");
+            A a = new A();
+            a.Show(log);
+        }
+    }
+
+    class A
+    {
+        public void Show(ILog log)
+        {
+            log.Info("来自A的输出方法名测试");
+        }
+    }
+}
