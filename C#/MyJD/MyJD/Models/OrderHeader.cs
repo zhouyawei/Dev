5a5e3cbfe79127e595735c81f74385adedd609e5
zhouyawei <1072817424@qq.com>
2016-09-27 11:27:47 +0800

    订单

--- C#/MyJD/MyJD/Models/OrderHeader.cs
+++ C#/MyJD/MyJD/Models/OrderHeader.cs
@@ -8,44 +8,44 @@ using System.Web;
 
 namespace MyJD.Models
 {
-    [DisplayName("訂單主檔")]
+    [DisplayName("订单详情")]
     [DisplayColumn("DisplayName")]
     public class OrderHeader
     {
         [Key]
         public int Id { get; set; }
 
-        [DisplayName("訂購會員")]
+        [DisplayName("订购会员")]
         [Required]
         public Member Member { get; set; }
 
         [DisplayName("收件人姓名")]
-        [Required(ErrorMessage = "請輸入收件人姓名，例如: +886-2-23222480#6342")]
-        [MaxLength(40, ErrorMessage = "收件人姓名長度不可超過 40 個字元")]
-        [Description("訂購的會員不一定就是收到商品的人")]
+        [Required(ErrorMessage = "请输入收件人姓名，例如: +886-2-23222480#6342")]
+        [MaxLength(40, ErrorMessage = "收件人姓名长度不可超过 40 个字符")]
+        [Description("订购的会员不一定就是收到商品的人")]
         public string ContactName { get; set; }
 
-        [DisplayName("聯絡電話")]
-        [Required(ErrorMessage = "請輸入您的聯絡電話，例如: +886-2-23222480#6342")]
-        [MaxLength(25, ErrorMessage = "電話號碼長度不可超過 25 個字元")]
+        [DisplayName("联系电话")]
+        [Required(ErrorMessage = "请输入您的联系电话，例如: +886-2-23222480#6342")]
+        [MaxLength(25, ErrorMessage = "电话号码长度不可超过25个字符")]
         [DataType(DataType.PhoneNumber)]
         public string ContactPhoneNO { get; set; }
 
-        [DisplayName("遞送地址")]
-        [Required(ErrorMessage = "請輸入商品遞送地址")]
+        [DisplayName("运送地址")]
+        [Required(ErrorMessage = "请入商品运送地址")]
         public string ContactAddress { get; set; }
 
-        [DisplayName("訂單金額")]
+        [DisplayName("订单金额")]
         [Required]
         [DataType(DataType.Currency)]
-        [Description("由於訂單金額可能會受商品遞送方式或優惠折扣等方式異動價格，因此必須保留購買當下算出來的訂單金額")]
+        [Description("由于订单金额可能会受商品递送方式或优惠折扣等方式引起价格波动，因此必须保留购买当时算出来的订单金额")]
         public decimal TotalPrice { get; set; }
 
-        [DisplayName("訂單備註")]
+        [DisplayName("订单备注")]
         [DataType(DataType.MultilineText)]
         public string Memo { get; set; }
 
-        [DisplayName("訂購時間")]
+        [DisplayName("订购时间")]
         public DateTime BuyOn { get; set; }
 
         [NotMapped]
