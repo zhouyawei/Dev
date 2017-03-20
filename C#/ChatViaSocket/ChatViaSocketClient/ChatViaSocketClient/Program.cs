baf485515f45a0adc2633e19f311fc4f86418eaf
zhouyawei <1072817424@qq.com>
2017-03-20 20:29:21 +0800

    Socket

---
+++ C#/ChatViaSocket/ChatViaSocketClient/ChatViaSocketClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatViaSocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            int remotePort = 1569;
+            string remoteIPInString = "127.0.0.1";
+            EndPoint remotEndPoint = new IPEndPoint(IPAddress.Parse(remoteIPInString), remotePort);
+            clientSocket.Connect(remotEndPoint);
+
+            byte[] buffer = new byte[4096];
+            string content = "Hello Danting";
+            byte[] messagesInBytes = Encoding.UTF8.GetBytes(content);
+            Array.Copy(messagesInBytes, buffer, messagesInBytes.Length);
+            int bytesSend = clientSocket.Send(buffer, messagesInBytes.Length, SocketFlags.None);
+
+            clientSocket.Close();
+        }
+    }
+}
---
+++ C#/ChatViaSocket/ChatViaSocketServer/ChatViaSocketServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatViaSocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            int localPort = 1569;
+            EndPoint localEndPoint = new IPEndPoint(IPAddress.Any, localPort);
+            serverSocket.Bind(localEndPoint);
+            int backlog = 10000;
+            serverSocket.Listen(backlog);
+
+            Socket clientSocket = serverSocket.Accept();
+            byte[] buffer = new byte[4096];
+            int bytesRead = clientSocket.Receive(buffer);
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            Console.WriteLine(message);
+
+            clientSocket.Close();
+
+            serverSocket.Close();
+
+        }
+    }
+}
